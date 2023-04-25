86b1305d33007d78983f8118a1413d67e9d9c395
Matías Fernández <61636811+MFernandez0992@users.noreply.github.com>
2023-04-25 16:06:48 -0300

    Finish

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+    './src/**/*.{vue,js,ts}'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: theme => ({
+        'wave-pattern': 'url(../img/wave-white.png)',
+        'wave-pattern-dots': 'url(../img/pattern-white-dots.png)',
+        'pattern-characters-red': 'url(../img/pattern-characters-red.png)',
+        'character-mario': 'url(../img/character-l.png)',
+        'wave-pink': 'url(../img/wave-pink.png)',
+        'yellow-dots': 'url(../img/pattern-yellow-dots.png)',
+      }),
+      screens: {
+        'xsm': '320px'
+      }
+    },
+  },
+  plugins: [],
+}
